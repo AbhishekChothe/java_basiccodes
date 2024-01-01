acf6a4c21c9ef52f781b44999bd3c48c01874ebd
Abhishekchothe <abhishekchothe20@gmail.com>
2024-01-01 22:36:54 +0530

    today we are adding new file

---
+++ src/Lab003.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Lab003 {
+    public static void main(String[] args) {
+        // write our code
+        byte age = 1;
+        age = 127;
+        System.out.println(age+1);
+        //
+        System.out.println(age);
+
+        // add age with the value
+        System.out.println("Age is "+age);
+    }
+}
---
+++ src/lab001.java
@@ -0,0 +1,8 @@
+package src;
+
+public class lab001 {
+    public static void main(String[] args) {
+        System.out.println("Hey");
+    }
+
+}
